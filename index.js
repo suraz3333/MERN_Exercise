135400c535265ccf919a451488534c5c006355ad
Suraj <surazlaw3@gmail.com>
2024-01-07 12:23:11 +0545

    initial step

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require ('express')
+const app = express()
+app.use (express.json())
+const PORT =3001
+let contact=[
+    {"id":1,
+    "name":"Arto Hellas",
+    "number":"2344"
+    },
+    {
+        "id":2,
+        "name":"Ada lovelace",
+        "number":"23490904"
+    },
+    {
+        "id":3,
+        "name":"Dan Abromove",
+        "number":"23490904" 
+    }
+]
+app.get(`/api/persons`,(request,response)=>
+    {response.json(contact)})
+app.listen(PORT,()=>console.log(`Server running on ${PORT}`))
